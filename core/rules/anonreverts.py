be9c8eed6c348cbc7f8741d6bbc95d443b9c67a3
4shadoww <4shadoww0@gmail.com>
2017-05-29 20:26:28 +0300

    commit

--- core/rules/anonreverts.py
+++ core/rules/anonreverts.py
@@ -5,28 +5,32 @@ class YunoModule:
 	name = "anonreverts"
 	cfg_ver = None
 
-	config = {
-		"hours": 1,
-		"reverts_required": 2,
-		"score": 1,
-	}
+	config = [
+		{
+			"hours": 1,
+			"reverts_required": 2,
+			"score": 1,
+			"groups": ["autoconfirmed"]
+		}
+	]
 
 	def run(self, rev):
 		site = pywikibot.Site()
-		edits = op.createEditList(rev["title"], end_hours=self.config["hours"])
-		reverts = op.getRevertList(edits, end_hours=self.config["hours"])
 
-		ip_reverts = 0
+		for rule in self.config:
+			edits = op.createEditList(rev["title"], end_hours=rule["hours"])
+			reverts = op.getRevertList(edits, end_hours=rule["hours"])
+			ip_reverts = 0
 
-		if len(reverts) >= self.config["reverts_required"]:
-			for revert in reverts:
-				reverter = pywikibot.User(site, title=revert["reverter"])
-				victim = pywikibot.User(site, title=revert["victim"])
+			if len(reverts) >= rule["reverts_required"]:
+				for revert in reverts:
+					reverter = pywikibot.User(site, title=revert["reverter"])
+					victim = pywikibot.User(site, title=revert["victim"])
 
-				if "autoconfirmed" not in victim.groups() or "autoconfirmed" not in reverter.groups():
-					ip_reverts += 1
+					if all(i not in victim.groups() for i in rule["groups"]) or  all(i not in reverter.groups() for i in rule["groups"]):
+						ip_reverts += 1
 
-		if ip_reverts >= self.config["reverts_required"]:
-			return self.config["score"]
+		if ip_reverts >= rule["reverts_required"]:
+			return rule["score"]
 
 		return 0
---
+++ readme.md
@@ -0,0 +1,2 @@
+# stabilizerbot
+Stabilizerbot is a bot for mediawiki FlaggedRevs extension that stabilizes articles automaticially when specific rules are met.
