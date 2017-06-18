66afd55fa700e06d9e09c15ae96deb3bb54075c8
4shadoww <4shadoww0@gmail.com>
2017-06-18 15:23:08 +0300

    commit

--- yuno.py
+++ yuno.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 # Import python modules
-import sys
 import os
+import sys
 
 # Import path tool
 from core import path
 
-# Append lib path
-sys.path.append(path.main()+"core/lib/")
-
 # Set pywikibot config path
 os.environ["PYWIKIBOT2_DIR"] = path.main()
+
+# Append lib path
+sys.path.append(path.main()+"core/lib/")
 
 # Import core modules
 from core import worker
