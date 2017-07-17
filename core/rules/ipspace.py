e46d770b2598b8036e15c85924c14d0fc4b3c02c
4shadoww <4shadoww0@gmail.com>
2017-07-17 15:03:43 +0300

    added new rule modules

---
+++ core/rules/ipspace.py
@@ -0,0 +1,47 @@
+import json
+import ipaddress
+
+from core.rule_core import *
+from core import yapi
+
+class YunoModule:
+
+	name = "ipspace"
+	cfg_ver = None
+
+	config = {
+		"expiry": 24,
+		"score": 1,
+		"list_path": "Käyttäjä:VakauttajaBot/greylist.json"
+	}
+
+	list_ver = None
+	api = yapi.MWAPI
+	greylist = None
+
+	def run(self, rev):
+		score = 0
+		expiry = None
+
+		lastrev = self.api.getLatestRev(self.config["list_path"])
+
+		if not lastrev:
+			logger.critical("greylist not found")
+			return score, expiry
+
+		if lastrev != self.list_ver:
+			self.greylist = json.loads(self.api.getText(self.config["list_path"]))
+			self.list_ver = lastrev
+
+		for address in self.greylist["values"]:
+			try:
+				user = ipaddress.ip_address(rev["user"])
+			except ValueError:
+				continue
+
+			if user in ipaddress.ip_network(address):
+				score = self.config["score"]
+				expiry = self.config["expiry"]
+				break
+
+		return score, expiry
---
+++ core/rules/pagelist.py
@@ -0,0 +1,42 @@
+import json
+
+from core.rule_core import *
+from core import yapi
+
+class YunoModule:
+
+	name = "pagelist"
+	cfg_ver = None
+
+
+	config = {
+		"expiry": 24,
+		"score": 1,
+		"list_path": "Käyttäjä:VakauttajaBot/greylist.json"
+	}
+
+	list_ver = None
+	api = yapi.MWAPI
+	greylist = None
+
+	def run(self, rev):
+		score = 0
+		expiry = None
+
+		lastrev = self.api.getLatestRev(self.config["list_path"])
+
+		if not lastrev:
+			logger.critical("greylist not found")
+			return score, expiry
+
+		if lastrev != self.list_ver:
+			self.greylist = json.loads(self.api.getText(self.config["list_path"]))
+			self.list_ver = lastrev
+
+		for title in self.greylist["values"]:
+			if title.replace("_", "") == rev["title"]:
+				score = self.config["score"]
+				expiry = self.config["expiry"]
+				break
+
+		return score, expiry
